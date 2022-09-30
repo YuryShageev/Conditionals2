fdf87cabca2d79472e5ca21a049f686b8c394c5f
YuryShageev <schageev.yur@yandex.ru>
2022-09-30 10:44:49 +0300

    Второй урок по условным операторам

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        System.out.println("Task 1");
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        //Задача 2
+        System.out.println("Task 2");
+        int clientDeviceYear = 2014;
+        if (clientDeviceYear >= 2015) {
+            if (clientOS == 1) {
+                System.out.println("Приложение будет работать корректно на вашей системе iOS");
+            } else if (clientOS == 0) {
+                System.out.println("Приложение будет работать корректно на вашей системе Android");
+        }
+        } else if (clientDeviceYear < 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию Android по ссылке");
+            } else if (clientOS == 1) {
+                System.out.println("Установите облегченную версию iOS по ссылке");
+            }
+        }
     }
 }
