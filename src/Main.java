1be290cfefdb5fe591c41d9c0832d66f86ea7f4e
YuryShageev <schageev.yur@yandex.ru>
2022-09-30 16:25:09 +0300

    Второй урок по условным операторам

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,43 @@ public class Main {
 
         //Задача 4
         System.out.println("Task 4");
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        if (deliveryDistance > 20) {
+            deliveryDays ++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDays ++;
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
 
+        //Задача 5
+        System.out.println("Task 5");
+        int monthNumber = 8;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Некорректный месяц!");
+        }
     }
 }
